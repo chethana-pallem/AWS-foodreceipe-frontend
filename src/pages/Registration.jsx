04ade5bc7b590aaa1df28a39a2e6c2a4a302a239
PALLEM CHETHANA <chetnapallem2006@gmail.com>
2025-09-22 12:15:28 +0530

    Update Registration.jsx

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -40,7 +40,7 @@ const Registration = () => {
 
     // console.log('Registration data:', formData);
 
-    axios.post('http://localhost:8082/register', formData).then((response) => {
+    axios.post('http://52.91.185.69:8082/register', formData).then((response) => {
       alert(response.data);
       navigate('/login');
     })
