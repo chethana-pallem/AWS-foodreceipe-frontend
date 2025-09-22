161587b75534e84cdd732eecd7ca560e802a261d
PALLEM CHETHANA <chetnapallem2006@gmail.com>
2025-09-22 12:13:50 +0530

    Update RecipeDetails.jsx

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -17,7 +17,7 @@ export default function RecipeDetails() {
 
   const fetchRecipeDetails = async () => {
     try {
-      const response = await axios.get("http://localhost:8082/recipe");
+      const response = await axios.get("http://52.91.185.69:8082/recipe");
       const allRecipes = response.data;
       const foundRecipe = allRecipes.find(r => r.rid.toString() === rid);
       if (foundRecipe) {
